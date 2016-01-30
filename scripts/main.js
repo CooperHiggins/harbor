63ef8941ed15d25baddbd3a5b58a0fa398066533
CooperHiggins <cooperhiggins@gmail.com>
2016-01-30 15:26:29 -0600

    update

---
+++ scripts/main.js
@@ -0,0 +1,87 @@
+console.log('Welcome To Harbor Christian/Leadership Academy'); // eslint-disable-line no-console
+
+
+
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(30.5151747,-97.8882121),
+    zoom:18,
+    mapTypeId:google.maps.MapTypeId.HYBRID
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+$(document).ready(function () {
+  //rotation speed and timer
+  var speed = 5000;
+  
+  var run = setInterval(rotate, speed);
+  var slides = $('.slide');
+  var container = $('#slides ul');
+  var elm = container.find(':first-child').prop("tagName");
+  var item_width = container.width();
+  var previous = 'prev'; //id of previous button
+  var next = 'next'; //id of next button
+  slides.width(item_width); //set the slides to the correct pixel width
+  container.parent().width(item_width);
+  container.width(slides.length * item_width); //set the slides container to the correct total width
+  container.find(elm + ':first').before(container.find(elm + ':last'));
+  resetSlides();
+  
+  
+  //if user clicked on prev button
+  
+  $('#buttons a').click(function (e) {
+    //slide the item
+    
+    if (container.is(':animated')) {
+      return false;
+    }
+    if (e.target.id == previous) {
+      container.stop().animate({
+        'left': 0
+      }, 1500, function () {
+        container.find(elm + ':first').before(container.find(elm + ':last'));
+        resetSlides();
+      });
+    }
+    
+    if (e.target.id == next) {
+      container.stop().animate({
+        'left': item_width * -2
+      }, 1500, function () {
+        container.find(elm + ':last').after(container.find(elm + ':first'));
+        resetSlides();
+      });
+    }
+    
+    //cancel the link behavior      
+    return false;
+    
+  });
+  
+  //if mouse hover, pause the auto rotation, otherwise rotate it  
+  container.parent().mouseenter(function () {
+    clearInterval(run);
+  }).mouseleave(function () {
+    run = setInterval(rotate, speed);
+  });
+  
+  
+  function resetSlides() {
+    //and adjust the container so current is in the frame
+    container.css({
+      'left': -1 * item_width
+    });
+  }
+  
+});
+//a simple function to click next link
+//a timer will call this function, and the rotation will begin
+
+function rotate() {
+  $('#next').click();
+}
